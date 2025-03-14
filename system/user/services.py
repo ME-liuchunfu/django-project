4e02302a01412a254a0ea16ff59111c2b41def4f
spring <429829320@qq.com>
2025-03-14 19:26:54 +0800

    lg

--- system/user/services.py
+++ system/user/services.py
@@ -21,17 +21,8 @@ class MetaVo:
         if data:
             self.data.update(data)
 
-    def __setattr__(self, key, value):
-        self.__setattr__(key, value)
-
-    def __getattr__(self, item):
-        return self.__getattr__(item)
-
     def __str__(self):
         return self.data.__str__()
-
-    def __call__(self):
-        return self.data
 
 
 class RouterVo:
@@ -49,37 +40,32 @@ class RouterVo:
             'children': []
         }
         if data:
-            self.__setattr__('name', data.get('name'))
-            self.__setattr__('path', data.get('path'))
-            self.__setattr__('hidden', data.get('hidden'))
-            self.__setattr__('redirect', data.get('redirect'))
-            self.__setattr__('component', data.get('component'))
-            self.__setattr__('query', data.get('query'))
-            self.__setattr__('alwaysShow', data.get('alwaysShow'))
-            self.__setattr__('meta', data.get('meta'))
-            if 'hidden' in data and not isinstance(data.get('hidden'), bool):
-                self.data['hidden'] = data.get('hidden') == '1'
+            self.data['name'] = get_route_name(data)
+            self.data['path'] = get_route_name(data)
+            self.data['hidden'] = data.get('visible')
+            self.data['component'] = get_component(data)
+            self.data['query'] = data.get('query')
+            link = data.get('path') if is_http(data.get('path')) else None
+            self.set_meta(MetaVo(
+                {
+                'title': data.get('menu_name'),
+                'icon': data.get('icon'),
+                'noCache': data.get('is_cache') == '1',
+                'link': link
+                }
+            ))
+            if 'visible' in data and not isinstance(data.get('visible'), bool):
+                self.data['hidden'] = data.get('visible') == '1'
+
+    def data_value(self, key, value):
+        self.data[key] = value
 
 
-    def set_meta(self, meta: MetaVo):
-        self.__setattr__('meta', meta)
-
-
-    def __setattr__(self, key, value):
-        self.__setattr__(key, value)
-
-
-    def __getattr__(self, item):
-        return self.__getattr__(item)
-
+    def set_meta(self, meta: MetaVo = None):
+        self.data['meta'] = meta
 
     def __str__(self):
         return self.data.__str__()
-
-
-    def __call__(self):
-        return self.data
-
 
 
 def get_routers(user_id:int) -> list:
@@ -88,18 +74,164 @@ def get_routers(user_id:int) -> list:
     else:
         menus_list = __get_menu_tree_by_user_id(user_id)
 
+    menus_list = get_child_perms(menus_list, 0)
     tree_datas = build_tree_menus(menus_list)
     return tree_datas
+
+
+def get_child_perms(menus_list: list, parent_id: int) -> list:
+    ret_data_list = []
+    for menu in menus_list:
+        if menu.get('parent_id') == parent_id:
+            recursion_fn(menus_list, menu)
+            ret_data_list.append(menu)
+
+    return ret_data_list
+
+
+def recursion_fn(menus_list: list, menu):
+    # 得到子节点列表
+    child_list = get_child_list(menus_list, menu)
+    menu['children'] = child_list
+    for child in child_list:
+        if has_child(menus_list, child):
+            recursion_fn(menus_list, child)
+
+
+def get_child_list(menus_list: list, menu) -> list:
+    ret_list = []
+    for menus in menus_list:
+        if menus.get('parent_id') == menu.get('menu_id'):
+            ret_list.append(menus)
+
+    return ret_list
+
+
+def has_child(menus_list: list, menu) -> bool:
+    return len(get_child_list(menus_list, menu)) > 0
 
 
 def build_tree_menus(menus_list: list) -> list:
     tree_datas = []
     for menu in menus_list:
         router_vo = RouterVo(menu)
+        children = menu.get('children')
+        if children and len(children) > 0 and "M" == menu.get('menu_type'):
+            router_vo.data_value('redirect', 'noRedirect')
+            router_vo.data_value('alwaysShow', True)
+            router_vo.data_value('children', build_tree_menus(children))
+        elif is_menu_frame(menu):
+            router_vo.set_meta(None)
+            children_list = []
+            router = RouterVo()
+            router.data_value('path', menu.get('path'))
+            router.data_value('component', menu.get('component'))
+            router.data_value('name', get_route_name(menu))
+            router.set_meta(MetaVo({
+                'title': menu.get('menu_name'),
+                'icon': menu.get('icon'),
+                'noCache': "1" == menu.get('is_cache'),
+                'link': menu.get('path')
+            }))
+            children_list.append(router)
+            router_vo.data_value('children', children_list)
+
+        elif menu.get('parent_id') == 0 and is_inner_link(menu):
+            router_vo.set_meta(MetaVo({'title': menu.get('menu_name'), 'icon': menu.get('icon')}))
+            router_vo.data_value('path', '/')
+            children_list = []
+            router = RouterVo()
+            router_path = inner_link_replace_each(menu.get('path'))
+            router.data_value('path', router_path)
+            router.data_value('component', "InnerLink")
+            router.data_value('name', get_route_name(menu))
+            router.set_meta(MetaVo({
+                'title': menu.get('menu_name'),
+                'icon': menu.get('icon'),
+                'link': menu.get('path')
+            }))
+            children_list.append(router)
+            router_vo.data_value('children', children_list)
 
         tree_datas.append(router_vo)
 
+
     return tree_datas
+
+
+class Constants:
+    HTTP = "http://"
+    HTTPS = "https://"
+    WWW = "www."
+    DOT = "."
+    COLON = ":"
+
+
+def inner_link_replace_each(path):
+    """
+    Replace specific substrings in the given URL path.
+
+    :param path: The URL path to be modified
+    :return: Modified URL path
+    """
+    path = path.replace(Constants.HTTP, "")
+    path = path.replace(Constants.HTTPS, "")
+    path = path.replace(Constants.WWW, "")
+    path = path.replace(Constants.DOT, "/")
+    path = path.replace(Constants.COLON, "/")
+    return path
+
+def get_component(menu: dict):
+    component = "Layout"
+    if menu.get('component') and not is_menu_frame(menu):
+        component = menu.get('component')
+    elif not menu.get('component') and menu.get('parent_id') != 0 and is_inner_link(menu):
+        component = "InnerLink"
+    elif not menu.get('component') and is_parent_view(menu):
+        component = "ParentView"
+    return component
+
+
+def is_parent_view(menu: dict) -> bool:
+    return menu.get('parent_id') != 0 and "M" == menu.get('menu_type')
+
+
+def is_inner_link(menu: dict) -> bool:
+    return menu.get('is_frame') == "1" and is_http(menu.get('path'))
+
+
+def is_http(val: str) -> bool:
+    if not val:
+        return False
+    return val.startswith('http:') or val.startswith('https:')
+
+
+def is_menu_frame(menu: dict) -> bool:
+    return menu.get('parent_id') == 0 and "C" == menu.get('menu_type') and menu.get('is_frame') == "1"
+
+
+def get_route_name(menu: dict):
+    """
+    Get the route name from the menu information.
+
+    :param menu: SysMenu object containing menu information
+    :return: The route name
+    """
+    if menu.get('is_frame'):
+        return ""
+    return format_route_name(menu.get('route_name'), menu.get('path'))
+
+
+def format_route_name(name, path):
+    """
+    Get the route name, or use the route path if the route name is not set.
+
+    :param name: The route name
+    :param path: The route path
+    :return: The route name in capitalized format
+    """
+    router_name = name if name else path
+    return router_name.capitalize()
 
 
 def get_role_permission(user_id: int, username: str) -> tuple[str]:
